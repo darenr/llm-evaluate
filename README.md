6ade993f1d1c0aff7521bbef74a6640926345c4b
Daren Race <daren.race@gmail.com>
2024-02-21 14:00:40 -0800

    first commit, simple example

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # llm-evaluate
+
 Evaluate LLM and produce a eval card and html report
+
+## Useful links
+
+- [ ] https://huggingface.co/docs/evaluate/en/a_quick_tour#visualization
---
+++ simple.py
@@ -0,0 +1,35 @@
+import evaluate
+import pandas as pd
+from statistics import mean
+from typing import Optional, Dict
+
+
+def score_dataset(
+    df: pd.DataFrame,
+    *,
+    metric: str,
+    predictions_col: str,
+    references_col: str,
+    evaluate_args: Optional[Dict[str, str]] = None,
+):
+    results = evaluate.load(metric).compute(
+        predictions=df[predictions_col],
+        references=df[references_col],
+        **(evaluate_args if evaluate_args else {}),
+    )
+
+    print(
+        f"Mean f1: {mean(results.f1)}. over {len(df)} observations",
+    )
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("data/candidates_references.csv")
+
+    score_dataset(
+        df,
+        metric="bertscore",
+        predictions_col="candidate",
+        references_col="reference",
+        evaluate_args={"lang": "en", "model_type": "distilbert-base-uncased"},
+    )
